d80952880f52f216481f090a3a316fdb4c4f3d77
Edgar Uriel Jimenez Gonzalez <edgar.u.j@hotmail.com>
2021-01-26 06:33:24 +0000

    Implemented correct output to channel. Only one task after that code breaks.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import schedule 
-import time
+import asyncio
+import aioschedule as  schedule 
 import os
 from discord.ext import commands
+import time
 
 bot= commands.Bot(command_prefix='.')
 
@@ -10,61 +11,57 @@ async def on_ready():
   print("Bot is ready")
 
 async def event(ctx, eventName):
-  await ctx.send("@everyone we have the event"+eventName+"Don't fortget to join in")
-  await ctx.send("test")
+  await ctx.send("@everyone we have the event "+eventName+" Don't fortget to join in")
+  #await ctx.send("test")
   print("test")
-
-def nothing(t):
-  return 0
+  return schedule.CancelJob
 
 @bot.command()
 async def now(ctx):
-  schedule.every(1).seconds.do(nothing,await event(ctx, "OW"))
+  schedule.every(5).seconds.do( event, ctx, "test")
   while 1:
-      schedule.run_pending()
-      time.sleep(1)
-
+      await schedule.run_pending() 
 
 
 @bot.command()
 async def addEvent(ctx, arg1, arg2, arg3):
   if(arg2 =="monday"):
-    schedule.every().monday.at(arg3).do(nothing, await event(ctx,arg1))
+    schedule.every().monday.at(str(arg3)).do( event, ctx,arg1)
     await ctx.send("Event created {} Monday at {} " .format(arg1, arg3))
     
 
   if(arg2 == "tuesday"):
-    schedule.every().tuesday.at(arg3).do(nothing, await event(ctx,arg1))
+    schedule.every().tuesday.at(str(arg3)).do(event,ctx, arg1)
     await ctx.send("Event created {} Tuesday at {} " .format(arg1, arg3))
   
 
   if(arg2 == "wednesday"):
-    schedule.every().wednesday.at(arg3).do(nothing, await event(ctx,arg1))
+    schedule.every().wednesday.at(str(arg3)).do( event,ctx,arg1)
     await ctx.send("Event created {} Wednesday at {} " .format(arg1, arg3))
   
 
   if(arg2 == "thursday"):
-    schedule.every().thursday.at(arg3).do(nothing, await event(ctx,arg1))
+    schedule.every().thursday.at(str(arg3)).do( event,ctx,arg1)
     await ctx.send("Event created {} Thursday at {} " .format(arg1, arg3))
    
 
   if(arg2 == "friday"):
-    schedule.every().friday.at(arg3).do(nothing, await event(ctx,arg1))
+    schedule.every().friday.at(str(arg3)).do( event,ctx,arg1)
     await ctx.send("Event created {} Friday at {} " .format(arg1, arg3))
   
 
   if(arg2 == "saturday"):
-    schedule.every().saturday.at(arg3).do(nothing, await event(ctx,arg1))
+    schedule.every().saturday.at(str(arg3)).do( event,ctx,arg1)
     await ctx.send("Event created {} Saturday at {} " .format(arg1, arg3))
    
 
   if(arg2 == "sunday"):
-    schedule.every().sunday.at(arg3).do(nothing, await event(ctx,arg1))
+    schedule.every().sunday.at(str(arg3)).do(event,ctx,arg1)
     await ctx.send("Event created {} Sunday at {} " .format(arg1, arg3))
   
-  while 1:
-      schedule.run_pending()
-      time.sleep(1)
+  
+  while True:
+    await schedule.run_pending()
 
 
 
